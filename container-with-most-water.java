e7e65d3bde3bd34560ba183451b72870500587b6
Leave-it-blank <70465598+Leave-it-blank@users.noreply.github.com>
2023-04-09 14:10:36 +0530

    Update container-with-most-water.java

--- container-with-most-water.java
+++ container-with-most-water.java
@@ -1,25 +1,42 @@
 //https://leetcode.com/problems/container-with-most-water/description/
 
-//need to fix time complixity
-
+//time complixity of o(n)
 class Solution {
     public int maxArea(int[] height) {
         
-        int len = height.length;
-        int max =0 , i, j, x;
-        for( i=0; i< len -1 ; i++){
-            for( j=i +1; j< len; j++ ){
-                if(height[i] < height[j]){
-                    x = height[i] * (j-i);
-                   }
-                   else {
-                     x = (height[j] * (j-i));
-                    }
-                  if(max < x ){
-                    max = x;
-                    }
+        // int len = height.length;
+        // int max =0 , i, j, x;
+        // for( i=0; i< len -1 ; i++){
+        //     for( j=i +1; j< len ; j++ ){
+        //         if(height[i] < height[j]){
+        //             x = height[i] * (j-i);
+        //            }
+        //            else {
+        //              x = (height[j] * (j-i));
+        //             }
+        //           if(max < x ){
+        //             max = x;
+        //             }
                
-            }
+        //     }
+        // }
+
+        // return max;
+
+        int len = height.length -1;
+        int max=0, left =0, right = len;
+
+        while(right> left){
+                int h = Math.min(height[left] , height[right]);
+                int w = right-left;
+                int area = w*h;
+                max = Math.max(area, max);
+
+                if(height[right]> height[left]){
+                    left ++;
+                }else {
+                    right --;
+                }
         }
 
         return max;
