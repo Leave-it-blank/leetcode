5c6a1b2bd3f39707f2514de1dbba8bb05fd5b2e2
Leave-it-blank <70465598+Leave-it-blank@users.noreply.github.com>
2023-05-01 00:00:29 +0530

    Update irst-bad-version.java

--- irst-bad-version.java
+++ irst-bad-version.java
@@ -1,1 +1,22 @@
 //https://leetcode.com/problems/first-bad-version/?envType=study-plan&id=algorithm-i
+
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+       int left=0,right=n,ans=n;
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            if(isBadVersion(mid))
+            {
+                ans=mid;
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+                }
+        }
+        return ans;
+       
+    }
+}
